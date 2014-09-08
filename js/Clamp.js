26cf2162a535c69c20c8960aacd20bccdbcd4d84
Alexandre <bighal.cormier@gmail.com>
2014-09-08 19:59:44 +0200

    Update Clamp.js

--- js/Clamp.js
+++ js/Clamp.js
@@ -1,4 +1,5 @@
-	function Clamp($min,$max){ //pas terminé
+	function Clamp($dimension,$min,$max,$range,$round){ 
+	
 		//engine
 		this.type="clamp"
 		this.serial=EG.generateSerial();
@@ -6,28 +7,66 @@
 		this.engine="";
 		this.min = $min;
 		this.max = $max;
+		this.range = $range;
 		EG.add(this);		
 		
 		//input
 		this.input = new Array();
+		
 		this.setInput = function($i){
 			this.input=$i;
 		}
+		
+		this.setRange = function($r){
+			this.range=$r;
+		}
+		
+		this.setMax = function($m){
+			this.max=$m;
+		}		
+		
+		this.setMin = function($m){
+			this.min=$m;
+		}	
+		
 		//output
 		this.output=0;
 		this.getOutput = function(){
 			return this.calcul(this.input)
 		}
+		this.getLimit = function(){
+			
+		}
+		
 		//calcul
 		this.calcul = function ($n){
-			if($n>this.min&&$n<this.max ){
-				return $n;
+			var n = $n;
+			var result = 0;
+			
+			switch($dimension){
+			
+				case '1d':
+				
+					result = ( ( n - this.min ) / this.max ) * this.range;
+					
+				break;
+				case '2d':					
+					result = {
+					
+						x : ( ( n.x - this.min.x ) / this.max.x ) * this.range.x,
+						y : ( ( n.y - this.min.y ) / this.max.y ) * this.range.y
+						
+					}					
+				break;
 			}
-			if($n>this.max){
-				return this.max;
+			
+			if($round){
+			
+				return Math.round(result);
+				
 			}
-			if($n<this.min){
-				return this.min;
-			}
+			
+			return result;
+			
 		}
 	}
